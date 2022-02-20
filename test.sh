1291d957bbe19259bacb75303873c4bacb1fac18
Göktuğ Özdemir <goktug.ozdemir@atrosbt.com>
2022-02-20 18:20:05 +0300

    Test.sh color improvement

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
-echo "Flutter is cleaning..."
+echo "\033[0;33mFlutter is cleaning...\033[0m"
 flutter clean
-echo "Flutter packages are getting..."
+echo "\033[0;33mFlutter packages are getting...\033[0m"
 flutter pub get
-echo "Old coverage is removing..."
+echo "\033[0;33mOld coverage is removing...\033[0m"
 rm -rf coverage
-echo "Test coverage is generating..."
+echo "\033[0;33mTest coverage is generating...\033[0m"
 flutter test --coverage
-echo "Test coverage badge is generating..."
+echo "\033[0;33mTest coverage badge is generating...\033[0m"
 flutter pub run test_coverage_badge
-echo "Test coverage HTML file is generating..."
+echo "\033[0;33mTest coverage HTML file is generating...\033[0m"
 genhtml -o coverage coverage/lcov.info
-echo "Test coverage HTML page is opening..."
+echo "\033[0;33mTest coverage HTML page is opening...\033[0m"
 open coverage/index.html
